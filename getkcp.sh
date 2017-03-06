e32c3831132aada522348e6e81c0e511c01b662c
supppig <supppig@163.com>
2017-03-06 19:42:26 +0800

    Add files via upload

---
+++ getkcp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+url=$(wget -q -O- https://api.github.com/repos/xtaci/kcptun/releases/latest | grep "rowser_download_url" | grep "linux-amd64" | cut -d'"' -f4)
+echo $url
+wget -O kcp.tar.gz ${url}
+tar zxvf kcp.tar.gz
+echo "OK"
+
