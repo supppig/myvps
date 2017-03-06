d5a0d109bf1158f89e241318dd09b5867389a4d3
supppig <supppig@163.com>
2017-03-06 15:39:31 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,34 +1,7 @@
-export /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-#nohup /usr/sbin/sshd -D >/dev/null &
-cd /root
-mkdir kcp
-cd kcp
-url=`wget --no-check-certificate -q -O- https://api.github.com/repos/xtaci/kcptun/releases/latest | grep -m1 'assets_url' | cut -d'"' -f4`
-downloadurl=`wget --no-check-certificate -q -O- $url | grep 'browser_download_url' | grep 'linux-amd64' | cut -d'"' -f4`
-wget -q -O kcp.tar.gz $downloadurl
-tar zxf kcp.tar.gz
-cat > kcp-server.json << 'EOF'
-{
-    "listen":":4000",
-    "target":"127.0.0.1:8388",
-	"key":"supppig",
-	"crypt":"none",
-	"mode":"fast",
-	"nocomp":true
-}
-EOF
+#!/bin/bash
+
+cd /root/kcp
 nohup ./server_linux_amd64 -c kcp-server.json >/dev/null &
 
-cd /root
-git clone -b manyuser https://github.com/shadowsocksr/shadowsocksr.git ssr
-cd ssr
-cp ./config.json ./user-config.json
-sed -ri 's/^.*\"password\".*/    \"password\": \"supppig\",/' ./user-config.json
-sed -ri 's/^.*\"method\".*/    \"method\": \"rc4-md5\",/' ./user-config.json
-sed -ri 's/^.*\"protocol\".*/    \"protocol\": \"auth_sha1_v2_compatible\",/' ./user-config.json
-sed -ri 's/^.*\"obfs\".*/    \"obfs\": \"http_simple_compatible\",/' ./user-config.json
-sed -ri 's/^.*\"fast_open\".*/    \"fast_open\": false/' ./user-config.json
-nohup python ./shadowsocks/server.py -d start
-
-
-
+cd /root/ssr
+nohup python ./shadowsocks/server.py -d start >/dev/null &
