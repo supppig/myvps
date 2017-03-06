976c0b9e34e7ea32b5a6a921ed02eceafaa7cdbb
supppig <supppig@163.com>
2017-03-06 20:07:56 +0800

    Update getkcp.sh

--- getkcp.sh
+++ getkcp.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 export PATH='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
-cd /root
+cd /root/kcp
 url=$(wget -q -O- https://api.github.com/repos/xtaci/kcptun/releases/latest | grep "rowser_download_url" | grep "linux-amd64" | cut -d'"' -f4)
-echo $url
-wget -O kcp.tar.gz ${url}
+wget -q -O kcp.tar.gz ${url}
 tar zxvf kcp.tar.gz
-ls
-echo "OK"
-
