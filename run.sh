3989faeb815d2858004e38ecaecfc77a17cf3cb1
supppig <supppig@163.com>
2017-03-23 21:16:56 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@
 cd /root/kcp
 nohup ./server_linux_amd64 -c kcp-server.json >/dev/null &
 
-cd /root/ssr
-python ./shadowsocks/server.py -d start
+nohup ss-server -s 0.0.0.0 -p 8388 -k supppig -m rc4-md5 >/dev/null &
 
 /usr/sbin/sshd -D
