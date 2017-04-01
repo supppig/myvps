667873c79cb05190444719b3a15cba986c3bffc8
supppig <supppig@163.com>
2017-04-01 09:22:24 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@
 
 cd /root/kcp
 nohup ./server_linux_amd64 -c kcp-server.json >/dev/null &
-
+sleep 1
 nohup ss-server -s 0.0.0.0 -p 8388 -k supppig -m rc4-md5 >/dev/null &
-
+sleep 1
+nohup /root/doloop.sh >/dev/null &
+sleep 1
 /usr/sbin/sshd -D
